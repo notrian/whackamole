358c5334e8b47e63ba017531d9f147b4a44b75e7
rian <rnevcasey@gmail.com>
2023-02-16 11:29:05 -0500

    Pushing some code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 let score = 0;
+
+let wackamoleParent = document.getElementById('whack-a-mole');
+let holes = document.getElementsByClassName('hole');
+
+setInterval(() => {
+    for (let index = 0; index < holes.length; index++) {
+        const element = holes[index];
+        
+    }
+}, 5000)
+
+function getRandomHole() { 
+    return Math.floor(Math.random() * (9 - 1 + 1) + 1)
+}
